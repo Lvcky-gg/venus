c6b01e2072213559bf75e53c161914490de21487
John O'Donnell <matadorcrab@gmail.com>
2023-10-16 14:47:40 -0500

    set/get

--- src/main/java/com/example/learn/venus/models/Orbiter.java
+++ src/main/java/com/example/learn/venus/models/Orbiter.java
@@ -5,4 +5,36 @@ public class Orbiter {
     private String name;
     private int orbiterId;
     private String sponsor;
+
+    public OrbiterType getType() {
+        return type;
+    }
+
+    public void setType(OrbiterType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOrbiterId() {
+        return orbiterId;
+    }
+
+    public void setOrbiterId(int orbiterId) {
+        this.orbiterId = orbiterId;
+    }
+
+    public String getSponsor() {
+        return sponsor;
+    }
+
+    public void setSponsor(String sponsor) {
+        this.sponsor = sponsor;
+    }
 }
