e8be17a6169ec304571bc62b5c9a9cb3f50588b3
John O'Donnell <matadorcrab@gmail.com>
2023-10-17 10:09:13 -0500

    n/a

--- src/main/java/com/example/learn/venus/data/OrbiterFileRepository.java
+++ src/main/java/com/example/learn/venus/data/OrbiterFileRepository.java
@@ -3,6 +3,10 @@ import com.example.learn.venus.models.Orbiter;
 
 import com.example.learn.venus.models.OrbiterType;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrbiterFileRepository {
@@ -13,6 +17,24 @@ public class OrbiterFileRepository {
         this.filepath = filepath;
     }
     public List<Orbiter> findAll(){
-        return null;
+        ArrayList<Orbiter> result = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+            reader.readLine();
+            for(String line = reader.readLine(); line != null; line = reader.readLine()){
+                String[] fields = line.split(",");
+                if(fields.length == 4){
+                    Orbiter orbiter = new Orbiter();
+                    orbiter.setOrbiterId(Integer.parseInt(fields[0]));
+                    orbiter.setName(fields[1]);
+                    orbiter.setType(OrbiterType.valueOf(fields[2]));
+                    orbiter.setSponsor(fields[3]);
+                    result.add(orbiter);
+                }
+            }
+        }catch(IOException ex){
+            //nothing?
+
+        }
+        return result;
     }
 }
