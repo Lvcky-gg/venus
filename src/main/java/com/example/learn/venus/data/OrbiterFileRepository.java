b833af1eefd5dd7c1c7b5961e750ed8a2afc41d4
John O'Donnell <matadorcrab@gmail.com>
2023-10-17 09:49:29 -0500

    na

---
+++ src/main/java/com/example/learn/venus/data/OrbiterFileRepository.java
@@ -0,0 +1,10 @@
+package com.example.learn.venus.data;
+
+public class OrbiterFileRepository {
+
+    private final String filepath;
+
+    public OrbiterFileRepository(String filepath) {
+        this.filepath = filepath;
+    }
+}
