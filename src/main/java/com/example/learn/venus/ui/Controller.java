73702bc4b27af09d7c7a1aa55635149c0d1dab58
John O'Donnell <matadorcrab@gmail.com>
2023-10-19 13:36:10 -0500

    na

--- src/main/java/com/example/learn/venus/ui/Controller.java
+++ src/main/java/com/example/learn/venus/ui/Controller.java
@@ -14,6 +14,20 @@ public class Controller {
     public void run(){
         MenuOptions opts;
         do{
+            opts = view.displayMenuAndSelect();
+            System.out.println(opts.getTitle());
+            switch(opts){
+                case EXIT:
+                    break;
+                case DISPLAY_ORBITERS:
+                    break;
+                case CREATE_ORBITER:
+                    break;
+                case UPDATE_ORBITER:
+                    break;
+                case DELETE_ORBITER:
+                    break;
+            }
 
         }while(opts != MenuOptions.EXIT);
     }
--- src/main/java/com/example/learn/venus/ui/View.java
+++ src/main/java/com/example/learn/venus/ui/View.java
@@ -4,5 +4,8 @@ import java.util.Scanner;
 
 public class View {
     private final Scanner console = new Scanner(System.in);
+    public MenuOptions displayMenuAndSelect(){
+        return MenuOptions.EXIT;
+    }
 
 }
